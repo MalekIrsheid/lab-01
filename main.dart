96b235497f809c45719713fc28b6caea2c823b72
MalekIrsheid <malekirsheid2000@gmail.com>
2022-08-26 16:40:08 +0300

    New Update

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main() {
+  Map myInformation = {
+    "myName": "Malek",
+    "myAge": "22",
+    "city": "Irbid",
+    "myMajorInformation": ["MIS", "Yarmouk University", "Faculty of IT"],
+    "myFavouriteColors": ["black", "red", "white"]
+  };
+  String name = "malek";
+  double height = 175.5;
+
+  print(name.toUpperCase());
+  print(height.round());
+  print(myInformation);
+}
